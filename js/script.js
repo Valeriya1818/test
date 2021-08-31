4c404dc9d1bd626380e44711147bc2b29edacf89
Valeriya <malovav097@gmail.com>
2021-08-31 14:52:51 +0300

    test-1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+function test2(num) {
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+    for ( i in lookup ) {
+        while ( num >= lookup[i] ) {
+            roman += i;
+            num -= lookup[i];
+        }
+    }
+    console.log(roman);
+}
 
-function test(nums,target) {
+function test1(nums,target) {
 
     let result = [];
 
@@ -23,4 +33,7 @@ function test(nums,target) {
 let nums = [3,2,6,8,10],
     target = 12;
 
-test(nums,target);
+console.log('TEST-1. \'nums\' is '+JSON.stringify(nums)+' and \'target\' is '+target+', result will be: ');
+test1(nums,target);
+console.log('TEST-2. \'target\' is '+target+', result will be: ');
+test2(12);
